4561556154db63cd524acb97a36bf7293f671ee8
aslammaulana <aslammaulana1998@gmail.com>
2025-06-12 00:11:49 +0700

    1.1

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 // lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xxx.supabase.co';
-const supabaseKey = 'your-anon-key';
+const supabaseUrl = 'https://raoijaufniiwfljaqnqp.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhb2lqYXVmbmlpd2ZsamFxbnFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NTk2MDIsImV4cCI6MjA2NTIzNTYwMn0.NTd1DWgd9bYtQlX9J-oI6Ga4LRK3GGGY-bNry6v-WvY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
